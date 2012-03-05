fa729c83218a3ca49fa649bb3d4c1210737b123e
conn0146 <connell.connect@gmail.com>
2012-03-05 12:54:27 -0500

    -Edited DB to work PHPfog

--- open-data-app/includes/db.php
+++ open-data-app/includes/db.php
@@ -3,7 +3,7 @@
 // This is the best way to keep usernames and passwords out of the public GitHub repos
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
-$dsn = getenv('DB_DSN');
+$dsn = stripslashes(getenv('DB_DSN'));
 
 // Opens a connection to the database and stores it in a variable
 // PDO allows us to connect to different types of databases, not just MySQL
