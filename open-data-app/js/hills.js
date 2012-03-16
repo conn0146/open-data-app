f427b143acc9abb77a2747641dc1166d003d09ad
conn0146 <connell.connect@gmail.com>
2012-03-16 11:48:24 -0400

    Changed JS to include image for pin, added tester image to test script

--- open-data-app/js/hills.js
+++ open-data-app/js/hills.js
@@ -13,39 +13,32 @@ $(document).ready(function () {
 	$('.hills li').each(function (i, elem) {
 		var hill = $(this).find('a').html();
 
-		// Create some HTML content for the info window
-		// Style the content in your CSS
 		var info = '<div class="info-window">'
 			+ '<strong>' + hill + '</strong>'
 			+ '</div>'
 		;
 
-		// Determine this dino's latitude and longitude
 		var lat = $(this).find('meta[itemprop="latitude"]').attr('content');
 		var lng = $(this).find('meta[itemprop="longitude"]').attr('content');
 		var pos = new google.maps.LatLng(lat, lng);
 
-		// Create a marker object for this dinosaur
 		var marker = new google.maps.Marker({
 			position : pos
 			, map : map
 			, title : hill
-			, icon : 'images/bone.png'
+			, icon : 'images/snow.png'
 			, animation: google.maps.Animation.DROP
 		});
 
-		// A function for showing this dinosaur's info window
 		function showInfoWindow (ev) {
 			if (ev.preventDefault) {
 				ev.preventDefault();
 			}
 
-			// Close the previous info window first, if one already exists
 			if (infoWindow) {
 				infoWindow.close();
 			}
 
-			// Create an info window object and assign it the content
 			infoWindow = new google.maps.InfoWindow({
 				content : info
 			});
@@ -53,9 +46,8 @@ $(document).ready(function () {
 			infoWindow.open(map, marker);
 		}
 
-		// Add a click event listener for the marker
 		google.maps.event.addListener(marker, 'click', showInfoWindow);
-		// Add a click event listener to the list item
+
 		google.maps.event.addDomListener($(this).children('a').get(0), 'click', showInfoWindow);
 	});
 });
