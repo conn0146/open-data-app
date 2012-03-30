17a69bd4a98dd423fb3a4c9c167860eeb091759b
conn0146 <connell.connect@gmail.com>
2012-03-30 10:53:16 -0400

    Fixed missing JS link

--- includes/theme-bottom.php
+++ includes/theme-bottom.php
@@ -1,5 +1,6 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDbQYTiRrft8Pi5JmZnvMj0s6THIrnfOxA&sensor=false"></script>
+	<script src="js/latlng.min.js"></script>
 	<script src="js/hills.js"></script>
 </body>
 </html>
