3a547d6e7eb1525cd540ccccbe5310a94cc6e3fa
conn0146 <connell.connect@gmail.com>
2012-03-30 10:57:38 -0400

    Added id to li for ratings

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ include 'includes/theme-top.php';
 			$rating = 0;
 		}
 	?>
-	<li itemscope itemtype="http://schema.org/TouristAttraction">
+	<li itemscope itemtype="http://schema.org/TouristAttraction" data-id="<?php echo $hills["id"]; ?>">
 		<a href="single.php?id=<?php echo $hills['id']; ?>" itemprop="name"><?php echo $hills['name']; ?></a>
 		<span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
 			<meta itemprop="latitude" content="<?php echo $hills['latitude']; ?>">
